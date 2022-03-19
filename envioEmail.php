553087193a2b887842b2c98d1201ac01094bec5f
uilsonps4 <uilsonps4@gmail.com>
2022-03-19 16:08:04 -0300

    Desafio3

--- envioEmail.php
+++ envioEmail.php
@@ -1,12 +1,12 @@
 <?php
 
 
-$name = utf8_encode ($_POST['name']);
+$nome = utf8_encode ($_POST['nome']);
 $cpf = utf8_encode ($_POST['cpf']);
 $email = utf8_encode ($_POST['email']);
 $mensagem = utf8_encode ($_POST['mensagem']);
 
-require 'PHPMailer/PHPMailerAutoload.php';
+require 'PHPMailer/PHPMailerAutoload.php'
 
 
 $mail = new PHPMailer;
@@ -23,15 +23,18 @@ $mail->Password = "Junior123";
 
 //Configuração da mensagem
 
-$mail->setFrom($mail->Username,"@Uilson Souza"); //Remetente
-$mail->addAddress ('uilsonsouzaphp@gmail.com'); //Destinatário
+$mail->setFrom($mail->Username,"Uilson Souza"); //Remetente
+$mail->addAddress ('uilsonps4@gmail.com'); //Destinatário
 $mail->Subject = "Fale com Conosco"; //Assunto do E-mail
 
 
-$conteudo_email = "Eu Passei no teste ?";
+$conteudo_email = "Você recebu uma mensagem de $nome com o registro $cpf ($email)
+<br><br>
+mensagem:<br>
+$mensagem";
 
 $mail->IsHTML(true);
-$mail->body = $conteudo_email;
+$mail->Body = $conteudo_email;
 
 if ($mail->send('')) {
     echo "E-mail enviado com sucesso!";
